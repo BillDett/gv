34e4ac2d30f44c5da76a9cf4cb64e65d9f26aa64
Bill Dettelback <billdett@gmail.com>
2021-02-21 14:40:22 -0500

    Navigating and editing

--- editor.go
+++ editor.go
@@ -1,27 +1,38 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/gdamore/tcell/v2"
 )
 
+//go:embed intro.txt
+var mediumtext string
+
 type editor struct {
-	x        int
-	y        int
-	width    int // How many runes wide is the editor
-	position int // Current rune underneath the cursor
-	cursX    int // X coordinate of the cursor
-	cursY    int // Y coordinate of the cursor
-	buf      *PieceTable
+	x          int
+	y          int
+	width      int         // How many runes wide is the editor
+	height     int         // How many lines high is the editor
+	position   int         // Current rune underneath the cursor
+	cursX      int         // X coordinate of the cursor
+	cursY      int         // Y coordinate of the cursor
+	buf        *PieceTable // Text being edited
+	lineIndex  []line      // Text Position index for each "line" after editor has been laid out.
+	linePtr    int         //index of the line currently beneath the cursor
+	topLine    int         // index of the topmost "line" of the window in lineIndex
+	bottomLine int         // index of the bottommost "line" of the window in lineIndex
+	startPos   int         // Text Position of first character in window
 }
 
-var editorWidth int // How wide is editor as % of terminal width
-
-var toprow int // topmost visible row in renderbuf
-var width int
-var height int
+// a line is a logic representation of a line that is rendered in the window
+type line struct {
+	position int // Text position in editor.text()
+	length   int // How many runes in this "line"
+}
 
 var defStyle tcell.Style
 
@@ -56,26 +67,55 @@ func drawBorder(s tcell.Screen, x, y, width, height int) {
 // TODO: This is going to need to get much smarter- we need to draw whitespace correctly (TAB, NL, CR) and also we
 //  should only render the text from the editor that fits on the screen's viewport.  Also, it should intelligently
 //  word wrap for us based on whitespace.
-func layoutEditorText(s tcell.Screen, runes *[]rune, width int) {
+func drawEditorText(s tcell.Screen, ed *editor, width int, height int) {
+	// Layout all of the text in the editor (re-create the lineIndex)
 	x := 1
 	y := 1
-	for _, r := range *runes {
-		s.SetContent(x, y, r, nil, defStyle)
+	runes := ed.text()
+	lineStartPos := 0
+	lineCount := 0
+	ed.lineIndex = []line{}
+	ed.lineIndex = append(ed.lineIndex, line{lineStartPos, -1})
+	for pos, r := range *runes {
 		x++
-		if x > width {
+		if x > width || r == '\n' { // wrap?
+			x--
+			ed.lineIndex[lineCount].length = x // Record length of the line we just saw
+			lineStartPos = pos + 1
+			ed.lineIndex = append(ed.lineIndex, line{lineStartPos, -1}) // Get ready for the next line
 			x = 1
 			y++
+			lineCount++
 		}
+	}
+	// handle remaining line if we had one
+	if x > 1 {
+		x--
+		ed.lineIndex[lineCount].length = x // Record length of the line we just saw
+	}
+
+	// Draw the rendered lines that should be visible in window (topline until end of window or text )
+	y = 1
+	for ed.bottomLine = ed.topLine; ed.bottomLine < len(ed.lineIndex) && y < height; ed.bottomLine++ {
+		x = 1
+		line := ed.lineIndex[ed.bottomLine]
+		for p := line.position; p < line.position+line.length; p++ {
+			if line.length != -1 {
+				s.SetContent(x, y, (*runes)[p], nil, defStyle)
+			}
+			x++
+		}
+		y++
 	}
 }
 
 func drawScreen(s tcell.Screen, ed *editor) {
-	width, height = s.Size()
-	editorWidth = int(float64(width) * 0.7)
+	width, height := s.Size()
+	editorWidth := int(float64(width) * 0.7)
 	s.Clear()
 	drawBorder(s, 0, 0, width, height)
 	s.ShowCursor(ed.cursX, ed.cursY)
-	layoutEditorText(s, ed.text(), editorWidth)
+	drawEditorText(s, ed, editorWidth, height-1)
 	s.Show()
 }
 
@@ -83,36 +123,92 @@ func newEditor(s tcell.Screen) *editor {
 	if s == nil {
 		return nil
 	}
-	width, height = s.Size()
+	width, height := s.Size()
 	ew := int(float64(width) * 0.7)
-	return &editor{5, 5, ew, 0, 1, 1, NewPieceTable("")}
+	return &editor{0, 0, ew, height - 2, 0, 1, 1, NewPieceTable(""), []line{}, 0, 0, 0, 0}
+}
+
+func (e *editor) dump() {
+	out := fmt.Sprintf("Width %d, Height %d, curX %d, curY %d\nPosition %d, Topline %d, Bottomline %d, startPos %d, #lines %d, linePtr %d, lineIndex %v\n",
+		e.width, e.height, e.cursX, e.cursY, e.position, e.topLine, e.bottomLine, e.startPos, len(e.lineIndex), e.linePtr, e.lineIndex)
+	ioutil.WriteFile("editordump.txt", []byte(out), 0644)
 }
 
 func (e *editor) moveRight() {
-	// TODO: Make sure we're still inside editor text
-	if e.cursX != e.width {
-		e.cursX++
-	} else {
-		e.cursX = 1
-		e.cursY++
+	if e.position < e.buf.lastpos {
+		if e.cursX != e.width {
+			e.cursX++
+		} else { // At end of a line, several things we can do here
+			// Are we at bottom right of window?
+			if e.cursX == e.width && e.cursY == e.height {
+				if e.linePtr < len(e.lineIndex)-1 { // More text to scroll down to
+					e.topLine++
+					e.cursX = 1
+					e.linePtr++
+				}
+			} else { // just at end of a line, move to next visible one
+				e.cursX = 1
+				e.cursY++
+				e.linePtr++
+			}
+		}
+		e.position++
 	}
-	e.position++
 }
 
 func (e *editor) moveLeft() {
-	// TODO: Make sure we're still inside editor text, handle beginning of line, etc
-	if e.cursX != 0 {
-		e.cursX--
-		e.position--
+	if e.position > 0 {
+		if e.cursX != 1 {
+			e.cursX--
+		} else { // At beginning of a line, several things we can do  here
+			// Are we at top left of window?
+			if e.cursX == 1 && e.cursY == 1 {
+				if e.linePtr > 0 { // More text to scroll up to
+					e.topLine--
+					e.linePtr--
+					e.cursX = e.lineIndex[e.linePtr].length
+				}
+			} else { // just at beginning of line, move to previous visible one
+				e.cursY--
+				e.linePtr--
+				e.cursX = e.lineIndex[e.linePtr].length
+			}
+		}
 	}
 }
 
 func (e *editor) moveUp() {
-
+	if e.linePtr > 0 {
+		if e.cursY == 1 { // Scroll
+			e.topLine--
+		} else if e.cursY > 0 {
+			e.cursY--
+		}
+		e.adjustCursX(-1)
+		e.position = e.lineIndex[e.linePtr].position + e.cursX - 1
+	}
 }
 
 func (e *editor) moveDown() {
+	if e.linePtr < len(e.lineIndex)-1 {
+		if e.cursY == e.height { // Scroll
+			e.topLine++
+		} else if e.cursY < e.height {
+			e.cursY++
+		}
+		e.adjustCursX(1)
+		e.position = e.lineIndex[e.linePtr].position + e.cursX - 1
+	}
+}
 
+// Helper function to 'adjust' the x cursor position based upon previous/next line length compared to where we're coming from
+func (e *editor) adjustCursX(offset int) {
+	currentLength := e.lineIndex[e.linePtr].length
+	e.linePtr += offset
+	newLength := e.lineIndex[e.linePtr].length
+	if newLength < currentLength && e.cursX > newLength {
+		e.cursX = newLength - 1
+	}
 }
 
 func (e *editor) addRune(r rune) {
@@ -125,9 +221,7 @@ func (e *editor) insertRuneAtCurrentPosition(r rune) {
 
 func (e *editor) insertRune(pos int, r rune) {
 	e.buf.InsertRunes(pos, []rune{r})
-	// TODO: Handle cursor wrapping at end of line, etc.
-	e.cursX++
-	e.position++
+	e.moveRight()
 }
 
 func (e *editor) addText(s string) {
@@ -138,12 +232,11 @@ func (e *editor) text() *[]rune {
 	return e.buf.Runes()
 }
 
+// TODO: Backspace is pretty broken
 func (e *editor) backspace() {
 	if e.position > 0 {
 		e.buf.Delete(e.position-1, 1)
-		e.position--
-		// TODO: handle cursor wrapping, etc
-		e.cursX--
+		e.moveLeft()
 	}
 }
 
@@ -157,22 +250,25 @@ func handleEvents(s tcell.Screen, ed *editor) {
 			switch ev.Key() {
 			case tcell.KeyEscape:
 				s.Fini()
+				ed.dump()
 				os.Exit(0)
 			case tcell.KeyDown:
 				ed.moveDown()
-				drawScreen(s, ed)
+				drawScreen(s, ed) // TODO: only if we have scrolled
 			case tcell.KeyUp:
 				ed.moveUp()
-				drawScreen(s, ed)
+				drawScreen(s, ed) // TODO: only if we have scrolled
 			case tcell.KeyRight:
 				ed.moveRight()
-				drawScreen(s, ed)
+				drawScreen(s, ed) // TODO: only if we have scrolled
 			case tcell.KeyLeft:
 				ed.moveLeft()
-				drawScreen(s, ed)
+				drawScreen(s, ed) // TODO: only if we have scrolled
 			case tcell.KeyBackspace, tcell.KeyBackspace2:
 				ed.backspace()
 				drawScreen(s, ed)
+			case tcell.KeyCtrlF: // for debugging
+				ed.dump()
 			case tcell.KeyRune:
 				ed.insertRuneAtCurrentPosition(ev.Rune())
 				drawScreen(s, ed)
@@ -199,7 +295,8 @@ func main() {
 	s.SetStyle(defStyle)
 
 	ed := newEditor(s)
-	ed.addText(lorem)
+	//ed.addText(lorem)
+	ed.addText(mediumtext)
 
 	drawScreen(s, ed)
 
